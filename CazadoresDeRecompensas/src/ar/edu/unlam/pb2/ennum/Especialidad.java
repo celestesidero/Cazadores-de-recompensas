db842235db4baed38efbb3f74b6d3b1d94ebdbaa
celestesidero <celestesidero@gmail.com>
2025-07-02 23:26:42 -0300

    Modificando el enum Especialidad para agregarle descripciones para los distintos tipos de cazadores

--- CazadoresDeRecompensas/src/ar/edu/unlam/pb2/ennum/Especialidad.java
+++ CazadoresDeRecompensas/src/ar/edu/unlam/pb2/ennum/Especialidad.java
@@ -1,6 +1,17 @@
 package ar.edu.unlam.pb2.ennum;
 
 public enum Especialidad {
-	    ENTORNO_CIVIL, ENTORNO_AGRESTRE, ENTORNO_DISCRETO;
+    ENTORNO_CIVIL("Entornos civiles"),
+    ENTORNO_AGRESTRE("Zonas agrestes"),
+    ENTORNO_DISCRETO("Capturas discretas");
 
+    private String descripcion;
+
+    private Especialidad(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
