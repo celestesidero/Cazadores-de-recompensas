e272dd70a806a053c91affab690614a46f929491
celestesidero <celestesidero@gmail.com>
2025-07-02 23:22:32 -0300

    Agregando a la clase CazadorRural comportamiento específico para captura e intimidación de los profugos

--- CazadoresDeRecompensas/src/ar/edu/unlam/pb2/CazadorRural.java
+++ CazadoresDeRecompensas/src/ar/edu/unlam/pb2/CazadorRural.java
@@ -1,5 +1,21 @@
 package ar.edu.unlam.pb2;
 
-public class CazadorRural extends Cazador{
+import ar.edu.unlam.pb2.ennum.Especialidad;
+
+public class CazadorRural extends Cazador {
+
+	public CazadorRural(String nombre, Integer experiencia) {
+		super(nombre, experiencia, Especialidad.ENTORNO_AGRESTRE);
+	}
+
+	@Override
+	public Boolean cumpleCondicionEspecificaDeCaptura(Profugo profugo) {
+		return profugo.getEsNervioso();
+	}
+
+	@Override
+	public void intimidarProfugo(Profugo profugo) {
+		profugo.setEsNervioso(true);
+	}
 
 }
