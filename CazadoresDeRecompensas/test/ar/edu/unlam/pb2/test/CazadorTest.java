407aca9a036eb80348132b31df069230f0ce590d
celestesidero <celestesidero@gmail.com>
2025-07-02 23:32:23 -0300

    Probando el manejo de excepciones en la creacion de los cazadores y su experiencia

--- CazadoresDeRecompensas/test/ar/edu/unlam/pb2/test/CazadorTest.java
+++ CazadoresDeRecompensas/test/ar/edu/unlam/pb2/test/CazadorTest.java
@@ -1,14 +1,54 @@
 package ar.edu.unlam.pb2.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import ar.edu.unlam.pb2.CazadorRural;
+import ar.edu.unlam.pb2.CazadorSigiloso;
+import ar.edu.unlam.pb2.CazadorUrbano;
 
 public class CazadorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void queElCazadorUrbanoSeaEspecialistaEnEntornosCiviles() {
+		CazadorUrbano cazadorUrbano = new CazadorUrbano("Esteban", 12);
+		String especialidadEsperada = "Entornos civiles";
+		assertEquals(especialidadEsperada, cazadorUrbano.getEspecialidad());
+	}
+
+	@Test
+	public void queElCazadorRuralSeaEspecialistaEnZonasAgrestes() {
+		CazadorRural cazadorRural = new CazadorRural("Carlos", 1);
+		String especialidadEsperada = "Zonas agrestes";
+		assertEquals(especialidadEsperada, cazadorRural.getEspecialidad());
+	}
+
+	@Test
+	public void queElCazadorSigilosoSeaEspecialistaEnCapturasDiscretas() {
+		CazadorSigiloso cazadorSigiloso = new CazadorSigiloso("Roberto", 3);
+		String especialidadEsperada = "Capturas discretas";
+		assertEquals(especialidadEsperada, cazadorSigiloso.getEspecialidad());
+	}
+
+	@Test
+	public void queLanceUnaExcepcionSiLaExperienciaQueSeIngresaAlCazadorEsNula() {
+		try {
+			new CazadorSigiloso("Roberto", null);
+		} catch (IllegalArgumentException e) {
+			String mensajeEsperado = "El valor que se ingreso no es valido. Debe ser un numero positivo";
+			assertEquals(mensajeEsperado, e.getMessage());
+		}
+	}
+
+	@Test
+	public void queLanceUnaExcepcionSiLaExperienciaQueSeIngresaAlCazadorEsNegativa() {
+		try {
+			new CazadorSigiloso("Alberto", -1);
+		} catch (IllegalArgumentException e) {
+			String mensajeEsperado = "El valor que se ingreso no es valido. Debe ser un numero positivo";
+			assertEquals(mensajeEsperado, e.getMessage());
+		}
 	}
 
 }
