0b1a4cf6e0857280cb9a12e317350a8a7c853a2f
celestesidero <celestesidero@gmail.com>
2025-07-02 23:23:45 -0300

    Agregando a la clase CazadorSigiloso comportamiento específico para captura e intimidación de los profugos

--- CazadoresDeRecompensas/src/ar/edu/unlam/pb2/CazadorSigiloso.java
+++ CazadoresDeRecompensas/src/ar/edu/unlam/pb2/CazadorSigiloso.java
@@ -1,5 +1,21 @@
 package ar.edu.unlam.pb2;
 
-public class CazadorSigiloso extends Cazador{
+import ar.edu.unlam.pb2.ennum.Especialidad;
+
+public class CazadorSigiloso extends Cazador {
+
+	public CazadorSigiloso(String nombre, Integer experiencia) {
+		super(nombre, experiencia, Especialidad.ENTORNO_DISCRETO);
+	}
+
+	@Override
+	public Boolean cumpleCondicionEspecificaDeCaptura(Profugo profugo) {
+		return profugo.getNivelHabilidad() < 50;
+	}
+
+	@Override
+	public void intimidarProfugo(Profugo profugo) {
+		profugo.setNivelHabilidad(profugo.getNivelHabilidad() - 5);
+	}
 
 }
