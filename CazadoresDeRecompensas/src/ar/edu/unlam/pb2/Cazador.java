ea0912fe27f46686d9c9e40f64d34459d396fef1
celestesidero <celestesidero@gmail.com>
2025-07-02 23:20:39 -0300

    Implementando estructura a la clase abstracta padre Cazador con validaciones, condición general de captura y método de intimidación; queda pendiente completar la lógica de procesoDeCaptura y manejo de experiencia, ademas de otros metodos

--- CazadoresDeRecompensas/src/ar/edu/unlam/pb2/Cazador.java
+++ CazadoresDeRecompensas/src/ar/edu/unlam/pb2/Cazador.java
@@ -1,5 +1,50 @@
 package ar.edu.unlam.pb2;
 
+import ar.edu.unlam.pb2.ennum.Especialidad;
+
 public abstract class Cazador {
+	private String nombre;
+	private Integer cantidadDeExperiencia;
+	private Especialidad especialidad;
+
+	public Cazador(String nombre, Integer experiencia, Especialidad especialidad) {
+		this.nombre = nombre;
+		this.verificarQueElIngresoDeExperienciaNoSeaNegativaNiNula(experiencia);
+		this.especialidad = especialidad;
+	}
+
+	public Boolean procesoDeCaptura(Profugo profugo) {
+		return null;
+	}
+
+	public Boolean cumpleCondicionGeneralDeCaptura(Profugo profugo) {
+		return this.cantidadDeExperiencia > profugo.getNivelInocencia();
+	}
+
+	public void ejecutarIntimidacionTotal(Profugo profugo) {
+		profugo.setNivelInocencia(profugo.getNivelInocencia() - 2);
+		this.intimidarProfugo(profugo);
+	}
+
+	public abstract Boolean cumpleCondicionEspecificaDeCaptura(Profugo profugo);
+
+	public abstract void intimidarProfugo(Profugo profugo);
+
+	private void aumentarExperiencia() {
+
+	}
+
+	private void verificarQueElIngresoDeExperienciaNoSeaNegativaNiNula(Integer experiencia) {
+		if (experiencia == null || experiencia <= 0) {
+			throw new IllegalArgumentException("El valor que se ingreso no es valido. Debe ser un numero positivo");
+		} else {
+			this.cantidadDeExperiencia = experiencia;
+		}
+
+	}
+
+	public String getEspecialidad() {
+		return this.especialidad.getDescripcion();
+	}
 
 }
