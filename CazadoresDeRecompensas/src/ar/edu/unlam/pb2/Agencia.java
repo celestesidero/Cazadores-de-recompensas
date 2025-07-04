9e2683a6d30ad0be2679dfc07571a0cba365c459
aguss002 <agustinacordoba002@gmail.com>
2025-07-04 18:54:15 -0300

    Funcionalidad y pruebas de la agencia

--- CazadoresDeRecompensas/src/ar/edu/unlam/pb2/Agencia.java
+++ CazadoresDeRecompensas/src/ar/edu/unlam/pb2/Agencia.java
@@ -1,5 +1,71 @@
 package ar.edu.unlam.pb2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Agencia {
+
+	private Map<String, Cazador> cazadores = new HashMap<>();
+	private List<Reporte> reportes = new ArrayList<>();
+	private final String nombre = "Agencia de seguridad especialistas en cazadores";
+
+	public Boolean agregarCazador(String id, Cazador cazador) {
+		return cazadores.put(id, cazador) == null ? true : false;
+	}
+
+	public Cazador buscarCazador(String id) {
+		return cazadores.get(id);
+	}
+
+	public Boolean registrarCaptura(Profugo profugo, Cazador cazador, Zona zona) {
+		return this.reportes.add(new Reporte(profugo, cazador, zona));
+	}
+
+	public Set<Profugo> obtenerTodosLosProfugosCapturados() {
+		Set<Profugo> capturados = new HashSet<Profugo>();
+		for (Reporte reporte : this.reportes)
+			capturados.add(reporte.getProfugo());
+		return capturados;
+	}
+
+	public Profugo obtenerProfugoMasHabilCapturado() {
+		Profugo masHabil = null;
+		for (Reporte reporte : this.reportes) {
+			Profugo actual = reporte.getProfugo();
+			if (masHabil == null || actual.getNivelHabilidad() > masHabil.getNivelHabilidad())
+				masHabil = actual;
+		}
+		return masHabil;
+	}
+
+	public Cazador obtenerCazadorConMasCapturas() {
+		Cazador cazadoresTop = null;
+		int maxCapturas = 0;
+		for (Map.Entry<Cazador, Integer> entry : contadorDeCapturas().entrySet()) {
+			if (entry.getValue() > maxCapturas) {
+				cazadoresTop = entry.getKey();
+				maxCapturas = entry.getValue();
+			} else if (entry.getValue() == maxCapturas && cazadoresTop != null) {
+				if (entry.getKey().getNombre().compareTo(cazadoresTop.getNombre()) < 0) {
+					cazadoresTop = entry.getKey();
+				}
+			}
+		}
+		return cazadoresTop;
+	}
+
+	private Map<Cazador, Integer> contadorDeCapturas() {
+		Map<Cazador, Integer> contadorCapturas = new HashMap<>();
+
+		for (Reporte reporte : reportes) {
+			Cazador cazador = reporte.getCazador();
+			contadorCapturas.put(cazador, contadorCapturas.getOrDefault(cazador, 0) + 1);
+		}
+		return contadorCapturas;
+	}
 
 }
--- CazadoresDeRecompensas/test/ar/edu/unlam/pb2/test/AgenciaTest.java
+++ CazadoresDeRecompensas/test/ar/edu/unlam/pb2/test/AgenciaTest.java
@@ -2,13 +2,116 @@ package ar.edu.unlam.pb2.test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import ar.edu.unlam.pb2.Agencia;
+import ar.edu.unlam.pb2.Cazador;
+import ar.edu.unlam.pb2.CazadorUrbano;
+import ar.edu.unlam.pb2.Profugo;
+import ar.edu.unlam.pb2.Zona;
+
 public class AgenciaTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private Agencia agencia;
+	private Cazador cazador;
+	private Cazador cazador1 = new CazadorUrbano("Marcos", 30);
+	private Cazador cazador2 = new CazadorUrbano("Miguel", 20);
+	private Profugo profugo;
+	private Profugo profugo1 = new Profugo("Brayan", 25, 10, false);
+	private Profugo profugo2 = new Profugo("Brayan", 25, 20, false);
+	private Zona zona;
+	
+	@Before
+	public void instanciandoAgencia() {
+		agencia = new Agencia();
 	}
-
+	
+	@Before
+	public void instanciandoZona() {
+		zona = new Zona("Rural");
+	}
+	
+	@Before
+	public void instanciandoCazador() {
+		cazador = new CazadorUrbano("Luis", 10);
+	}
+	
+	@Before
+	public void instanciandoProfugo() {
+		profugo = new Profugo("Brayan", 25, 1, false);
+	}
+	
+	@Test
+	public void agregarCazadorALaAgencia() {
+		assertTrue(agencia.agregarCazador("Luis", cazador));
+	}
+	
+	@Test
+	public void registrarUnaCaptura() {
+		assertTrue(agencia.registrarCaptura(profugo, cazador, zona));
+	}
+	
+	@Test
+	public void obtenerUnaListaDeProfugosCapturados() {
+		Set<Profugo> capturados = new HashSet<Profugo>();
+		preparacion(capturados);
+		assertEquals(capturados, agencia.obtenerTodosLosProfugosCapturados());
+	}
+	
+	@Test
+	public void obtenerAlProfugoCapturadoMasHabil() {
+		agregaAlregistroCapturas();
+		assertEquals(profugo2, agencia.obtenerProfugoMasHabilCapturado());
+	}
+	
+	@Test
+	public void obtenerAlCazadorConMasCapturasDeLaAgencia() {
+		agregaCazadoresYSusRegistros();
+		assertEquals(cazador2, agencia.obtenerCazadorConMasCapturas());
+	}
+	
+	@Test
+	public void obtenerAlCazadorConMasCapturasDeLaAgenciaConEmpate() {
+		agregaCazadoresYSusRegistrosEmpate();
+		assertEquals(cazador, agencia.obtenerCazadorConMasCapturas());
+	}
+	
+	private void agregaCazadoresYSusRegistros() {
+		agencia.agregarCazador("Luis", cazador);
+		agencia.agregarCazador("Marcos", cazador1);
+		agencia.agregarCazador("Miguel", cazador2);
+		agencia.registrarCaptura(profugo, cazador, zona);
+		agencia.registrarCaptura(profugo1, cazador2, zona);
+		agencia.registrarCaptura(profugo2, cazador2, zona);
+		agencia.registrarCaptura(profugo2, cazador2, zona);
+		agencia.registrarCaptura(profugo2, cazador1, zona);
+	}
+	
+	private void agregaCazadoresYSusRegistrosEmpate() {
+		agencia.agregarCazador("Luis", cazador);
+		agencia.agregarCazador("Marcos", cazador1);
+		agencia.agregarCazador("Miguel", cazador2);
+		agencia.registrarCaptura(profugo, cazador, zona);
+		agencia.registrarCaptura(profugo1, cazador, zona);
+		agencia.registrarCaptura(profugo2, cazador2, zona);
+		agencia.registrarCaptura(profugo2, cazador2, zona);
+		agencia.registrarCaptura(profugo2, cazador1, zona);
+	}
+	
+	private void agregaAlregistroCapturas() {
+		agencia.registrarCaptura(profugo, cazador, zona);
+		agencia.registrarCaptura(profugo1, cazador, zona);
+		agencia.registrarCaptura(profugo2, cazador, zona);
+	}
+	
+	private void preparacion(Set<Profugo> capturados) {
+		agregaAlregistroCapturas();
+		capturados.add(profugo);
+		capturados.add(profugo1);
+		capturados.add(profugo2);
+	}
 }
