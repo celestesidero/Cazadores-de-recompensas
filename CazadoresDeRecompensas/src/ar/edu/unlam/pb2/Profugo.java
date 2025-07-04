6de54f34c94bed3df3b61b5c0b2a972622ce6cca
aguss002 <agustinacordoba002@gmail.com>
2025-07-04 20:47:33 -0300

    Agrego funcionalidad para la evolucion

--- CazadoresDeRecompensas/src/ar/edu/unlam/pb2/Profugo.java
+++ CazadoresDeRecompensas/src/ar/edu/unlam/pb2/Profugo.java
@@ -2,48 +2,63 @@ package ar.edu.unlam.pb2;
 
 import java.util.Objects;
 
-import ar.edu.unlam.pb2.ennum.Entrenamiento;
-
-public class Profugo {
+public class Profugo implements Entrenamiento{
 //	ATRIBUTOS
+	private String nombre;
 	private Integer nivelInocencia;
 	private Integer nivelHabilidad;
 	private Boolean esNervioso;
-	private Entrenamiento tipo;
-	
+
 //  CONSTRUCTOR
-	public Profugo(Integer nivelInocencia, Integer nivelHabilidad, Boolean esNervioso, Entrenamiento tipo) {
-		setNivelInocencia(nivelInocencia);
-		setNivelHabilidad(nivelHabilidad);
+	public Profugo(String nombre, Integer nivelInocencia, Integer nivelHabilidad, Boolean esNervioso) {
+		this.nombre = nombre;
+		setNivelDeInocencia(nivelInocencia);
+		setNivelDeHabilidad(nivelHabilidad);
 		this.esNervioso = esNervioso;
-		this.tipo = tipo;
 	}
 	
 //	METODOS PUBLICOS
-	public void setNivelHabilidad(Integer nivelHabilidad) {
-		if(nivelHabilidad == null)
+	public Integer setNivelDeHabilidad(Integer nivelHabilidad) {
+		if (nivelHabilidad == null)
 			throw new NullPointerException("El nivel de habilidad no puede ser nulo");
-		else if(!validarHabilidad(nivelHabilidad))
+		else if (!validarHabilidad(nivelHabilidad))
 			throw new IllegalArgumentException("El nivel de habilidad debe estar entre 1 y 100");
-		else if(validarHabilidad(nivelHabilidad))
+		else if (validarHabilidad(nivelHabilidad))
 			this.nivelHabilidad = nivelHabilidad;
+		return this.nivelHabilidad;
 	}
-
-	public void setNivelInocencia(Integer nivelInocencia) {
-		if(nivelInocencia == null)
+	
+	public Integer setNivelDeInocencia(Integer nivelInocencia) {
+		if (nivelInocencia == null)
 			throw new NullPointerException("El nivel de inocencia no puede ser nulo");
-		else if(!validarInocencia(nivelInocencia))
+		else if (!validarInocencia(nivelInocencia))
 			throw new IllegalArgumentException("El nivel de inocencia debe estar entre 0 y 100");
-		else if(validarInocencia(nivelInocencia))
+		else if (validarInocencia(nivelInocencia))
 			this.nivelInocencia = nivelInocencia;
+		return this.nivelInocencia;
 	}
 	
 //	METODOS SOBRESCRITOS
 	@Override
-	public int hashCode() {
-		return Objects.hash(esNervioso, nivelHabilidad, nivelInocencia, tipo);
+	public void artesMarcialesAvanzadas() {
+		setNivelDeHabilidad(Math.min(this.nivelHabilidad * 2, 100));
+	}
+
+	@Override
+	public void elite() {
+		this.esNervioso = false;
+	}
+
+	@Override
+	public void proteccionLegal() {
+		setNivelDeInocencia(this.nivelInocencia < 40 ? 40 : this.nivelInocencia);
 	}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(esNervioso, nivelHabilidad, nivelInocencia);
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -54,34 +69,41 @@ public class Profugo {
 			return false;
 		Profugo other = (Profugo) obj;
 		return Objects.equals(esNervioso, other.esNervioso) && Objects.equals(nivelHabilidad, other.nivelHabilidad)
-				&& Objects.equals(nivelInocencia, other.nivelInocencia) && tipo == other.tipo;
+				&& Objects.equals(nivelInocencia, other.nivelInocencia);
 	}
-	
+
 //	SETTER Y GETTER
-	public void setEsNervioso(Boolean esNervioso) {
-		this.esNervioso = esNervioso;
-	}
-	
 	public Integer getNivelHabilidad() {
 		return this.nivelHabilidad;
 	}
-	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setEsNervioso(Boolean esNervioso) {
+		this.esNervioso = esNervioso;
+	}
+
 	public Integer getNivelInocencia() {
 		return nivelInocencia;
 	}
+
 	public Boolean getEsNervioso() {
 		return esNervioso;
 	}
 
-	//	METODOS PRIVADOS
+//  METODOS PRIVADOS
 	private boolean validarHabilidad(Integer numero) {
 		return numero >= 1 && numero <= 100;
 	}
-	
+
 	private Boolean validarInocencia(Integer numero) {
 		return (numero >= 0 && numero <= 100);
 	}
-	
-	
-	
+
 }
--- CazadoresDeRecompensas/test/ar/edu/unlam/pb2/test/ProfugoTest.java
+++ CazadoresDeRecompensas/test/ar/edu/unlam/pb2/test/ProfugoTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ar.edu.unlam.pb2.Profugo;
-import ar.edu.unlam.pb2.ennum.Entrenamiento;
 
 public class ProfugoTest {
 	
@@ -15,19 +14,45 @@ public class ProfugoTest {
 
 	@Before
 	public void inicializaProfugo() {
-		profugo = new Profugo(5, 10, true, Entrenamiento.ARTES_MARCIALES);
+		profugo = new Profugo("Brayan",5, 10, true);
 	}
 	
+	@Test
+	public void evolucionaEnArtesMarciales() {
+		profugo.artesMarcialesAvanzadas();
+		assertEquals(Integer.valueOf(20), profugo.getNivelHabilidad());
+	}
 	
 	@Test
-	public void fueIntimidadoDisminuyeSuNivelDeInocencia() {
-		
+	public void evolucionaEnArtesMarcialesYSuperaLos100() {
+		profugo.setNivelDeHabilidad(100);
+		profugo.artesMarcialesAvanzadas();
+		assertEquals(Integer.valueOf(100), profugo.getNivelHabilidad());
+	}
+	
+	@Test
+	public void evolucionaAEntrenamientoDeElite() {
+		profugo.elite();
+		assertEquals(false, profugo.getEsNervioso());
+	}
+	
+	@Test
+	public void evolucionaYObtenieneProteccionLegalCuandoSuNivelDeInocenciaEsMenorQue40() {
+		profugo.proteccionLegal();
+		assertEquals(Integer.valueOf(40), profugo.getNivelInocencia());
+	}
+	
+	@Test
+	public void evolucionaYObtenieneProteccionLegalCuandoSuNivelDeInocenciaEsMayorQue40() {
+		profugo.setNivelDeInocencia(60);
+		profugo.proteccionLegal();
+		assertEquals(Integer.valueOf(60), profugo.getNivelInocencia());
 	}
 	
 	@Test
 	public void lanzaUnaExceptionSiElNivelDeHabilidadEstaFueraDelRango() {
 		try {
-			profugo.setNivelHabilidad(0);
+			profugo.setNivelDeHabilidad(0);
 		} catch (IllegalArgumentException e) {
 			mensajeEsperado = "El nivel de habilidad debe estar entre 1 y 100";
 			assertEquals(mensajeEsperado, e.getMessage());
@@ -37,7 +62,7 @@ public class ProfugoTest {
 	@Test
 	public void lanzaUnaExceptionSiElNivelDeHabilidadEsNull() {
 		try {
-			profugo.setNivelHabilidad(null);
+			profugo.setNivelDeHabilidad(null);
 		} catch (NullPointerException e) {
 			mensajeEsperado = "El nivel de habilidad no puede ser nulo";
 			assertEquals(mensajeEsperado, e.getMessage());
@@ -46,14 +71,14 @@ public class ProfugoTest {
 	
 	@Test
 	public void elNivelDeHabilidadEsAsignado() {
-			profugo.setNivelHabilidad(20);
+			profugo.setNivelDeHabilidad(20);
 			assertEquals(Integer.valueOf(20), profugo.getNivelHabilidad());
 	}
 	
 	@Test
 	public void lanzaUnaExceptionSiElNivelDeInocenciaEstaFueraDelRango() {
 		try {
-			profugo.setNivelInocencia(-1);
+			profugo.setNivelDeInocencia(-7);
 		} catch (IllegalArgumentException e) {
 			mensajeEsperado = "El nivel de inocencia debe estar entre 0 y 100";
 			assertEquals(mensajeEsperado, e.getMessage());
@@ -63,7 +88,7 @@ public class ProfugoTest {
 	@Test
 	public void lanzaUnaExceptionSiElNivelDeInocenciaEsNull() {
 		try {
-			profugo.setNivelInocencia(null);
+			profugo.setNivelDeInocencia(null);
 		} catch (NullPointerException e) {
 			mensajeEsperado = "El nivel de inocencia no puede ser nulo";
 			assertEquals(mensajeEsperado, e.getMessage());
@@ -72,7 +97,7 @@ public class ProfugoTest {
 	
 	@Test
 	public void elNivelDeInocenciaEsAsignadoCuandoEstaDentroDelRango() {
-			profugo.setNivelInocencia(10);
+			profugo.setNivelDeInocencia(10);
 			assertEquals(Integer.valueOf(10), profugo.getNivelInocencia());
 	}
 	
